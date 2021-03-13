6d1284195707d87e313b3e723389e49a5bf088cd
Uduak Essien <acushla4real@gmail.com>
2021-03-13 18:40:35 +0100

    Update sign in component with google sign in props

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -3,6 +3,7 @@ import React from "react";
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 import "./sign-in.styles.scss";
+import { signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -49,7 +50,10 @@ class SignIn extends React.Component {
             label="password"
             required
           />
+          <div className="buttons">
           <CustomButton type="submit">Sign in</CustomButton>
+          <CustomButton onClick={ signInWithGoogle } isGoogleSignIn>Sign in with Google</CustomButton>
+          </div>
         </form>
       </div>
     );
