33caf3aabd8518ebb1d904cea591d1f4bec21f5f
Uduak Essien <acushla4real@gmail.com>
2021-03-23 17:15:54 +0100

    Style collection item component to have add to cart hover item

--- src/components/collection-item/collection-item.component.js
+++ src/components/collection-item/collection-item.component.js
@@ -1,4 +1,7 @@
 import React from "react";
+import CustomButton from "../custom-button/custom-button.component";
+
+import CustomeButton from '../custom-button/custom-button.component';
 
 import "./collection-item.styles.scss";
 
@@ -14,6 +17,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       <span className="name">{name}</span>
       <span className="price">${price}</span>
     </div>
+    <CustomButton inverted>Add to cart</CustomButton>
   </div>
 );
 
