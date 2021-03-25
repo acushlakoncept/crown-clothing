71e5dc04884ae5b3f3dbe3bc314d89770223a96f
Uduak Essien <acushla4real@gmail.com>
2021-03-25 14:14:52 +0100

    Move collection to its own component

---
+++ src/components/collections-overview/collections-overview.component.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import { selectCollections } from "../../redux/shop/shop.selector";
+
+import "./collections-overview.styles.scss";
+
+const CollectionOverview = ({ collections }) => (
+  <div className="collection-overview">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections
+});
+
+export default connect(mapStateToProps)(CollectionOverview);
