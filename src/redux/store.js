d18f495677a94371ca27cce3021cd68e1f3953a4
Uduak Essien <acushla4real@gmail.com>
2021-04-01 13:49:54 +0100

    Update shop redux reducer actions and types to use fetchcollections async func

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,19 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
+import thunk from 'redux-thunk';
 
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger];
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
 
-// export default { store, persistor };
+// export default { store, persistStore };
