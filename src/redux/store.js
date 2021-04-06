1ccea841536003b90cecd7b774be16832f6deeef
Uduak Essien <acushla4real@gmail.com>
2021-04-06 11:27:07 +0100

    Refactor store to use root-saga and add fetchCollectionsStart saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import logger from "redux-logger";
 import CreateSagaMiddleware from "redux-saga";
 
 import rootReducer from "./root-reducer";
-import { fetchCollectionsStart } from "./shop/shop.sagas";
+import rootSaga from "./root-saga";
 
 const sagaMiddleware = CreateSagaMiddleware();
 
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "development") {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-sagaMiddleware.run(fetchCollectionsStart);
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
