62f115f3c3fcbf68d2e9f1336c80d7974729d40a
Uduak Essien <acushla4real@gmail.com>
2021-04-12 08:25:55 +0100

    Update stripe-button to use the server api payment endpoint

--- client/src/components/stripe-button/stripe-button.component.js
+++ client/src/components/stripe-button/stripe-button.component.js
@@ -1,31 +1,47 @@
-import React from 'react';
-import StripeCheckout from 'react-stripe-checkout';
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
-import logo from '../../assets/crown.svg';
+import logo from "../../assets/crown.svg";
 
 const StripeCheckoutButton = ({ price }) => {
-    const priceForStripe = price * 100;
-    const publishableKey = 'pk_test_rk4bSlXSHiE7mIDhRn5QdO3c';
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_rk4bSlXSHiE7mIDhRn5QdO3c";
 
-    const onToken = token => {
-        console.log(token);
-        alert('Payment Successful');
-    }
+  const onToken = (token) => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then((reponse) => {
+        alert("Payment successful");
+      })
+      .catch((error) => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please be sure you use the provided credit card."
+        );
+      });
+  };
 
-    return (
-        <StripeCheckout
-          lable='Pay Now'
-          name='CROWN Clothing Ltd.'
-          billingAddress
-          shippingAddress
-          image={logo}
-          description={`Your total is $${price}`}
-          amount={priceForStripe}
-          panelLabel='Pay Now'
-          token={onToken}
-          stripeKey={publishableKey}
-          />
-    );
-}
+  return (
+    <StripeCheckout
+      lable="Pay Now"
+      name="CROWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image={logo}
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
 
 export default StripeCheckoutButton;
