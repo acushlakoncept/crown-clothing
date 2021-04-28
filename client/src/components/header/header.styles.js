8c0327010f9f71da23940d77b391627da3750c66
Uduak Essien <acushla4real@gmail.com>
2021-04-28 09:24:15 +0100

    Update app for mobile responsiveness

--- client/src/components/header/header.styles.js
+++ client/src/components/header/header.styles.js
@@ -7,12 +7,25 @@ export const HeaderContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 25px;
+
+  @media screen and (max-width: 800px) {
+    padding: 10px;
+    margin-bottom: 20px;
+    height: 60px;
+
+  }
 `;
 
 export const LogoContainer = styled(Link)`
   height: 100%;
   width: 70px;
   padding: 25px;
+
+  @media screen and (max-width: 800px) {
+    padding: 0;
+    width: 50px;
+
+  }
 `;
 
 export const OptionsContainer = styled.div`
@@ -21,6 +34,11 @@ export const OptionsContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+
+  @media screen and (max-width: 800px) {
+    width: 80%;
+
+  }
 `;
 
 export const OptionLink = styled(Link)`
